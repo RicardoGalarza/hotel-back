0e73a5484f1252a0508cec37dea6f8d94e8cb1b1
Ricardo Galarza <ricardogalarza1723@gmail.com>
2024-11-08 09:49:52 -0300

    probando sin sendgrid

--- src/main/java/com/example/demo/service/EmailService.java
+++ src/main/java/com/example/demo/service/EmailService.java
@@ -18,6 +18,7 @@ import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
 
+import jakarta.annotation.PostConstruct;
 
 @Service
 public class EmailService {
@@ -25,7 +26,13 @@ public class EmailService {
     @Value("${spring.sendgrid.api-key}")
     private String sendGridApiKey;
 
-    public void enviarCorreo(Reserva reserva, String destinatario, String nombreDestinatario, Habitacion habitacion, LocalDate fechaReserva, LocalDate fechaFinReserva) throws IOException {
+    @PostConstruct
+    public void init() {
+        System.out.println("SendGrid API Key: " + sendGridApiKey);
+    }
+
+    public void enviarCorreo(Reserva reserva, String destinatario, String nombreDestinatario, Habitacion habitacion,
+            LocalDate fechaReserva, LocalDate fechaFinReserva) throws IOException {
         Email from = new Email("caterinmorales111@gmail.com");
         Email to = new Email(destinatario);
 
@@ -46,42 +53,47 @@ public class EmailService {
             System.out.println("Body: " + response.getBody());
             System.out.println("Headers: " + response.getHeaders());
 
-            System.out.println("Se ha enviado el correo de confirmacion a : "+destinatario);
+            System.out.println("Se ha enviado el correo de confirmacion a : " + destinatario);
         } catch (IOException ex) {
             throw ex;
         }
     }
 
-    public String correoHtml(Reserva reserva, Habitacion habitacion, LocalDate fechaReserva, LocalDate fechaFinReserva, String nombreDestinatario) {
+    public String correoHtml(Reserva reserva, Habitacion habitacion, LocalDate fechaReserva, LocalDate fechaFinReserva,
+            String nombreDestinatario) {
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy", new Locale("es", "ES"));
 
         String contenidoHTML = "<!DOCTYPE html>" +
-            "<html>" +
-            "<head>" +
-            "<style>" +
-            "body { font-family: Arial, sans-serif; margin: 0; padding: 0; }" +
-            ".container { max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ddd; border-radius: 10px; }" +
-            ".header { background-color: #783200; color: white; padding: 10px; text-align: center; border-radius: 10px 10px 0 0; }" +
-            ".content { padding: 20px; }" +
-            ".button { display: inline-block; padding: 10px 20px; color: #ffffff; background-color: #783200; text-decoration: none; border-radius: 5px; margin-top: 20px; }" +
-            "</style>" +
-            "</head>" +
-            "<body>" +
-            "<div class='container'>" +
-            "<div class='header'><h2>Confirmación de Reserva</h2></div>" +
-            "<div class='content'>" +
-            "<p>"+nombreDestinatario.toUpperCase() + " gracias por tu reserva. Aquí tienes los detalles:</p>" +
-            "<p><strong>Habitación:</strong> " + habitacion.getNombre() + "</p>" +
-            "<p><strong>Fecha de inicio:</strong> " + fechaReserva.format(formatter) + "</p>" +
-            "<p><strong>Fecha de termino:</strong> " + fechaFinReserva.format(formatter) + "</p>" +
-            "<a href='http://hotel-back-production.up.railway.app/reserva/confirmar?reservaId=" + reserva.getId() + "' class='button' style='color:#fff'>Confirmar Reserva</a>" +
-            "</div>" +
-            "</div>" +
-            "</body>" +
-            "</html>";
-    
+                "<html>" +
+                "<head>" +
+                "<style>" +
+                "body { font-family: Arial, sans-serif; margin: 0; padding: 0; }" +
+                ".container { max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ddd; border-radius: 10px; }"
+                +
+                ".header { background-color: #783200; color: white; padding: 10px; text-align: center; border-radius: 10px 10px 0 0; }"
+                +
+                ".content { padding: 20px; }" +
+                ".button { display: inline-block; padding: 10px 20px; color: #ffffff; background-color: #783200; text-decoration: none; border-radius: 5px; margin-top: 20px; }"
+                +
+                "</style>" +
+                "</head>" +
+                "<body>" +
+                "<div class='container'>" +
+                "<div class='header'><h2>Confirmación de Reserva</h2></div>" +
+                "<div class='content'>" +
+                "<p>" + nombreDestinatario.toUpperCase() + " gracias por tu reserva. Aquí tienes los detalles:</p>" +
+                "<p><strong>Habitación:</strong> " + habitacion.getNombre() + "</p>" +
+                "<p><strong>Fecha de inicio:</strong> " + fechaReserva.format(formatter) + "</p>" +
+                "<p><strong>Fecha de termino:</strong> " + fechaFinReserva.format(formatter) + "</p>" +
+                "<a href='http://hotel-back-production.up.railway.app/reserva/confirmar?reservaId=" + reserva.getId()
+                + "' class='button' style='color:#fff'>Confirmar Reserva</a>" +
+                "</div>" +
+                "</div>" +
+                "</body>" +
+                "</html>";
+
         return contenidoHTML;
     }
-    
+
 }
