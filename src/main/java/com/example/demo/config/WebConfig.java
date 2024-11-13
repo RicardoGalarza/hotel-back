5b5390824fbbe4deee4cff5360fa76f101c01f90
Ricardo Galarza <ricardogalarza1723@gmail.com>
2024-11-13 16:18:02 -0300

    corregi me falto poner la s en el https

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://appealing-healing-production.up.railway.app")
+                .allowedOrigins("https://appealing-healing-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
