7845bf4d30d3a3211b0c8db43214987102cf85a8
Ricardo Galarza <ricardogalarza1723@gmail.com>
2024-11-13 16:12:58 -0300

    saque los cross de todo

--- src/main/java/com/example/demo/controller/HabitacionController.java
+++ src/main/java/com/example/demo/controller/HabitacionController.java
@@ -17,7 +17,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -42,7 +41,7 @@ import com.example.demo.service.HabitacionService;
 import com.example.demo.service.ImagenService;
 import com.example.demo.service.PoliticaService;
 
-@CrossOrigin(origins = "https://appealing-healing-production.up.railway.app")
+
 @RestController
 @RequestMapping("/habitaciones")
 public class HabitacionController {
