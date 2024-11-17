d1e001cc4595557a54f550194ab87c6acb57b745
Caterin Morales <caterin.morales5587@gmail.com>
2024-11-16 21:57:20 -0300

    .

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        System.out.println("Valor de GOOGLE_CREDENTIALS_JSON: " + System.getenv("GOOGLE_CREDENTIALS_JSON"));
         SpringApplication.run(Application.class, args);
     }
 }
