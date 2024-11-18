76f309e32c2ed9138f88307aef8cdfa191cdc67b
Ricardo Galarza <ricardogalarza1723@gmail.com>
2024-11-18 10:00:38 -0300

    cambiando el historial de reserva apra que capture la fecha que uno escogio en la reserva1

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,15 +1,11 @@
 package com.example.demo;
 
-import java.util.TimeZone;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(Application.class, args);
     }
 }
-
